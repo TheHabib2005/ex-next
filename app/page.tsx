dab5d7b284f091124d4e2dcb2ec46ac4570999f1
habib <mdwear2005@gmail.com>
2024-04-15 18:34:41 +0600

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 
 export default async function Home() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/users`);
-  const users = await data.json();
+  // const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/users`);
+  // const users = await data.json();
   return (
     <div>
-      {
+      {/* {
         users?.map((user: any) => {
           return <p key={user.id}>{user.name}</p>
         })
-      }
+      } */}
+      hello
     </div>
   )
 }
