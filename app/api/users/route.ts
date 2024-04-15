3d9b9f2d14b3a0372a9aa93431f6ef07fed5c84f
habib <mdwear2005@gmail.com>
2024-04-15 18:23:28 +0600

    first commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,17 +1,19 @@
-import {NextResponse} from "next/server"
-export const POST = async () =>{
-    try {
-        return NextResponse.json([{
-            id:1,
-            name:"habib"
-        },{
-            id:2,
-            name:"habib2"
-        },{
-            id:3,
-            name:"habib3"
-        }])
-    } catch (error) {
-        
-    }
-}
+import { NextResponse } from "next/server";
+export const GET = async () => {
+  try {
+    return NextResponse.json([
+      {
+        id: 1,
+        name: "habib1",
+      },
+      {
+        id: 2,
+        name: "habib2",
+      },
+      {
+        id: 3,
+        name: "habib3",
+      },
+    ]);
+  } catch (error) {}
+};
