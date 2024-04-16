3079c5e3d16cbf1110adc3f5b0e54ede2ee22642
habib <mdwear2005@gmail.com>
2024-04-16 12:01:40 +0600

    fd

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export const getData = async () => {
   let res = fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/users`, {
     method: "POST",
+
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
